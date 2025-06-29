4e7c83f7b7ed19a0c8d62d51038dce412c9ec7da
KienNgo22 <10423065@student.vgu.edu.vn>
2025-06-29 19:14:09 +0700

    Final Checking

---
+++ Assignment 9/src/config_public.h
@@ -0,0 +1,14 @@
+// config.example.h
+#pragma once
+
+// WiFi credentials
+const char* SSID = "Chickennuggets";
+const char* PASSWORD = "mot2ba4nam6";
+
+// AdafruitIO credentials
+const char* IO_USERNAME = "Chickennuggets";
+const char* IO_KEY = "mot2ba4nam6";
+
+// Feed names
+const char* SOIL_FEED_NAME = "soil-moisture";
+const char* RELAY_FEED_NAME = "relay-command";
